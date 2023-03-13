fcdebb4ec0bfef51593031c658430d63025156e4
Hajime <victoruyflores@hotmail.com>
2023-03-13 20:28:57 +0100

    chore: todo_list.py template completed

--- todo_list.py
+++ todo_list.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+pwd = "Victoruy10%40"
+user = "victor"
+uri = "mongodb+srv://" + user + ":" + pwd + "@cluster0.wozvku8.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri)
+
+database_name = 'todo'
+
+# Funciones de api a hacer.
+
+
+
+if __name__ == '__main__':
+    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
