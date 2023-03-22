4bcf47d4683d7172e15c3a6d3f3bd286ccf96200
Hajime <victoruyflores@hotmail.com>
2023-03-22 18:39:38 +0100

    feature: login & register working, missing api-key on header and the collection

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # todo_list
 
 ## Colleccions:
-### Users 
-### Llistas
-### Api-Key | formato uuid4
+1. Users 
+2. Llistas
+3. Api-Key | formato uuid4
 
 ## Objectes:
-### usuaris = {nom_usuari: "nick", nom: "josep", password: "1234"}
-### llistes = {"propietari": id_usuari, tasques: [{tasca_01}, ...]}
-### api_keys = {"usuari": id_usuari, api_keys: ["sdfl-sdf-asfdsaf-sfd", ...]}
+1. usuaris = {nom_usuari: "nick", nom: "josep", password: "1234"}
+2. llistes = {"propietari": id_usuari, tasques: [{tasca_01}, ...]}
+3. api_keys = {"usuari": id_usuari, api_keys: ["sdfl-sdf-asfdsaf-sfd", ...]}
--- todo_list.py
+++ todo_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 #-*- coding: utf-8 -*-
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
 
 app = Flask(__name__)
 pwd = "Victoruy10%40"
@@ -14,7 +13,52 @@ client = MongoClient(uri)
 
 database_name = 'todo'
 
-# Funciones de api a hacer.
+# Crea un nuevo usuario
+# curl -X POST -H "Content-Type: application/json" -d '{"nom":"Victor Alberto", "nickname":"victoruy", "password":"1234"}' http://localhost:5000/usuaris
+@app.route('/usuaris', methods=['POST'])
+def create_usuari():
+    data = request.get_json() # request the whole data
+    if not data:
+        return jsonify({"message": "Informacion no introducida"}), 400
+    usuari = {
+        "nom": data.get("nom"),
+        "nickname": data.get("nickname"),
+        "password": data.get("password"),
+    }
+    if client[database_name].usuaris.find_one({'nickname': usuari.get("nickname")}) is None:
+       client[database_name].usuaris.insert_one({'nom':usuari.get("nom"),'nickname':usuari.get("nickname"),'password':usuari.get("password")})
+       return "Created", 201
+    else: return "", 409
+
+# Muestra todo los usuarios
+@app.route('/usuaris',methods=['GET'])
+def get_usuaris():
+    usuaris = client[database_name].usuaris.find()
+    response = []
+    for usuari in usuaris:
+        response.append({
+            '_id': str(usuari['_id']),
+            'nickname': usuari['nickname'],
+            'nom': usuari['nom'],
+            'password': usuari['password']
+        })
+    return jsonify(response), 201
+
+# curl -X POST -H "Content-Type: application/json" -d '{"nickname":"victoruy", "password":"1234"}' http://localhost:5000/login
+@app.route('/login', methods=['POST'])
+def login_usuari():
+    data = request.get_json() # request the whole data
+    if not data:
+        return jsonify({"message": "Informacion no introducida"}), 400
+    usuari = {
+        "nickname": data.get("nickname"),
+        "password": data.get("password"),
+    }
+    find_user = client[database_name].usuaris.find_one({'nickname':usuari.get("nickname")})
+    if find_user and find_user["password"] == usuari.get("password"):
+        return jsonify({"message": "Logeado correctamente"}), 201
+    else:
+        return "Forbidden", 403
 
 
 
